037d49d70aa99ad25b1b67c3d9bcf23276c162cd
VTHARUN01 <v.tharun2589@gmail.com>
2022-10-18 22:02:33 +0530

    decoder/encoder jwt

--- src/tools/jwtDecoder/main.ts
+++ src/tools/jwtDecoder/main.ts
@@ -3,15 +3,20 @@ import jwt_decode from 'jwt-decode';
 import sign from 'jwt-encode';
 $('#jwt-decoded').on('input', ev => {
     const target = ev.target as HTMLInputElement;
-    const decodedHeader = jwt_decode(target.value, { header: true });
-    const decodedPayload = jwt_decode(target.value);
-    if (decodedHeader && decodedPayload) {
+    try {
+        const decodedHeader = jwt_decode(target.value, { header: true });
+        const decodedPayload = jwt_decode(target.value);
         $('#jwt-encoded').val(JSON.stringify({ header: decodedHeader, payload: decodedPayload }));
+    } catch (e) {
+        $('#jwt-encoded').val(e.message);
     }
 });
 
 $('#jwt-encoded').on('input', ev => {
     const target = ev.target as HTMLInputElement;
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    $('#jwt-decoded').val(sign(JSON.parse(target.value), 'secret'));
+    try {
+        $('#jwt-decoded').val(sign(JSON.parse(target.value), 'secret'));
+    } catch (e) {
+        $('#jwt-decoded').val(e.message);
+    }
 });
